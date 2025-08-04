04d7828dc5ce2be2a97a1dbace10b43fff072ee5
Aakriti Thapa <aakritithapa628@gmail.com>
2025-08-04 19:40:06 +0545

    changed index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
+
 app.use(cors({
   credentials: true,
   origin: process.env.FRONTEND_URL || 'http://localhost:5173' // Use env variable for flexibility
